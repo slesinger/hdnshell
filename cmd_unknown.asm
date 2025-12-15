144baef7d0aba84d31a0bc3c62522d41412a1d31
Honza Slesinger <slesinger@gmail.com>
2025-12-15 19:58:02 +0100

    fix cmd not fnd on empty line

--- cmd_unknown.asm
+++ cmd_unknown.asm
@@ -11,10 +11,14 @@
 //   - Does not execute any command.
 
 cmd_unknown:
-
-lda #$0D
-jsr CHROUT
+    pha  // save key
+    lda #KEY_RETURN  // TODO sometimes I receive $40 when RETURN is pressed after fiddling with history
+    jsr CHROUT
+    pla  // restore key
+    cmp #KEY_SPACE
+    beq !suppress_empty_line+
 
     ldy #MSG_UNKNOWN_COMMAND - MSGBAS    // display headers
     jsr SNDMSG
+!suppress_empty_line:
     CommandDone()  // jump to parser completion handler in parser.asm
